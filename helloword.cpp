5225a8af013c26aa4f9f297a44e899e55357dca0
frstlis <48125063@qq.com>
2019-12-09 20:35:39 +0800

    Update helloword.cpp

--- helloword.cpp
+++ helloword.cpp
@@ -1,11 +1,15 @@
 ```
   #include <iostream>
-  
   using namespace std;
-  
   int main(){
-  
       cout<<"Hello World C!!"<<endl;
       return 0;
       }
-      
+```
+```
+  #include <stdio.h>
+  int main(){
+      printf("Hello World C!!");
+      return 0;
+      }
+```
