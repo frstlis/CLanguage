399ea0a482039b44ae69e91736e3a4414f54125a
frstlis <48125063@qq.com>
2019-12-18 16:12:34 +0800

    Modify code

--- Code/helloword.cpp
+++ Code/helloword.cpp
@@ -1,15 +1,14 @@
-```
-  #include <iostream>
-  using namespace std;
-  int main(){
-      cout<<"Hello C++!!"<<endl;
-      return 0;
-      }
-```
-```
-  #include <stdio.h>
-  int main(){
-      printf("Hello C!!");
-      return 0;
-      }
-```
+#include <iostream>
+using namespace std;
+int main(){
+    cout<<"Hello C++!!"<<endl;
+    return 0;
+    }
+
+/*
+#include <stdio.h>
+int main(){
+    printf("Hello C!!");
+    return 0;
+    }
+*/
