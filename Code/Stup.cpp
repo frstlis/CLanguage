63641f4ccf0f9c43c75baeee8fd8d420ca45fe9f
frstlis <48125063@qq.com>
2019-12-19 16:33:31 +0800

    Addition Stup.cpp File

--- Code/Stup.cpp
+++ Code/Stup.cpp
@@ -1,20 +1,23 @@
 #include <iostream>
 #include <cstdio>
 int main() {
-    int n,x,y,z,w;					//x+y+z+w=n
+    int n,x,y,z,w,i;					//x+y+z+w=n
     while (scanf("%d",&n)!=EOF){
-        x=y=z=w=1;
-        for(x=1; x<n; x++){
-            for(y=1; y<n; y++){
-                for(z=1; z<n; z++){
-					for(w=1;w<n;w++){
+        i=x=y=z=w=0;
+        for(x=0; x<=n; x++){
+            for(y=0; y<=n; y++){
+                for(z=0; z<=n; z++){
+					for(w=0;w<=n;w++){
 						if(x+y+z+w==n){
-							printf("%d %d %d %d\n",x,y,z,w);
+							//printf("%d %d %d %d\n",x,y,z,w);
+							i++;
 						}
                     }
                 }
             }
         }
+    printf("%d",i);
     }
+    
     return 0;
 }
