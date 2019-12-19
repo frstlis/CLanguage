6240bd52202decfcc99b9a765cba515883fe0242
frstlis <48125063@qq.com>
2019-12-19 14:36:43 +0800

    Addition File ff.cpp

--- Code/ff.cpp
+++ Code/ff.cpp
@@ -5,5 +5,20 @@ using namespace std;
 
 
 int main(){
-
+	int a,b,sum=0;
+	cin>> a >> b;
+    if(a<b){              
+		for(int i=a;i<=b;i++){
+			sum+=i;
+		}
+	}else if(a>b){
+		for(int i=a;i>=b;i--){
+			sum+=i;
+		}
+	}else{
+		sum=a+b;
+	}
+	cout<< sum <<endl;
+		
+	return 0;
 }
