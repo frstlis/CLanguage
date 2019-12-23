a5cc0bcb1503afd403b7ffc1dedcb2898915dc1c
frstlis <48125063@qq.com>
2019-12-23 21:31:46 +0800

    Addition File avarice.c

---
+++ Code/avarice.c
@@ -0,0 +1,17 @@
+#include "stdio.h"
+
+struct node{
+	int st;
+	int ed;
+}a[100001];
+
+int main(){
+	int n;
+	//node a;
+	scanf("%d",&n);
+	for(int i=0;i<n;i++)
+		scanf("%d %d",&a[i].st,&a[i].ed);
+	for(int j=0;j<n;j++)
+		printf("%d %d \n",a[j].st,a[j].ed);
+	return 0;
+}
